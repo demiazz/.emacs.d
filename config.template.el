89aa7205284f420ee43abe9f881bc5a5ee4fdafb
Alexey Plutalov <demiazz.py@gmail.com>
2016-02-04 10:01:05 +0300

    Update `config.template.el`.

---
+++ config.template.el
@@ -0,0 +1,14 @@
+;;----- Layers
+
+;; Enable needed layers
+
+;; (demiazz/use-layers <layer-name> <layer-name> ...)
+
+;;----- Settings
+
+;; Customize layer settings
+
+;; (demiazz/settings <layer-name>
+;;                   <setting-name> <setting-value>
+;;                   <setting-name> <setting-value>
+;;                   ...)
