39da3b4fcb799b6307dbd0861e097f1074a894a9
Alexey Plutalov <demiazz.py@gmail.com>
2016-02-04 11:21:12 +0300

    Add support for `company-mode` to `go` layer.

--- layers/go.el
+++ layers/go.el
@@ -5,3 +5,13 @@
   :pin    "melpa-stable"
   :config
   (add-hook 'before-save-hook 'gofmt-before-save))
+
+(when (demiazz/layer-used-p 'company)
+  (use-package company-go
+    :ensure t
+    :pin    "melpa-stable"
+    :config
+    (add-hook 'go-mode-hook (lambda ()
+                              (set
+                               (make-local-variable 'company-backends)
+                               '(company-go))))))
